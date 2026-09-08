d150b7b7b5d1bad5cacbf3b0d786ac6cd52d0dd1
Rafael <rafael.o.silva30@aluno.senai.br>
2026-09-08 17:04:21 -0300

    feat: transforma a plataforma em aplicativo instalável

--- Web/src/main.jsx
+++ Web/src/main.jsx
@@ -2,3 +2,6 @@ import { createRoot } from 'react-dom/client';
 import './styles.css';
 import App from './App';
 createRoot(document.getElementById('root')).render(<App />);
+if ('serviceWorker' in navigator && import.meta.env.PROD) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'));
+}
